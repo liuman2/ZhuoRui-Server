8d47d72fb40b4a90e6d58057dfa02d0054eb8212
liuman2 <liuman2@163.com>
2016-11-15 00:41:13 +0800

    .

--- WebCenter.Web/Controllers/AnnualController.cs
+++ WebCenter.Web/Controllers/AnnualController.cs
@@ -59,7 +59,7 @@ namespace WebCenter.Web.Controllers
             return SuccessResult;
         }
 
-        public ActionResult Warning(int? customer_id)
+        public ActionResult Warning(int? customer_id, int? waiter_id, int? salesman_id, string name, string area)
         {
             var r = HttpContext.User.Identity.IsAuthenticated;
             if (!r)
@@ -83,7 +83,7 @@ namespace WebCenter.Web.Controllers
             var deptId = 0;
             int.TryParse(arrs[0], out userId);
             int.TryParse(arrs[2], out deptId);
-
+            
             var ops = arrs[4].Split(',');
             var hasInspect = ops.Where(o => o == "5").FirstOrDefault();
             var hasCompany = ops.Where(o => o == "1").FirstOrDefault();
@@ -105,42 +105,64 @@ namespace WebCenter.Web.Controllers
                 customerQuery1 = c => (c.customer_id == customer_id);
             }
 
-            //Expression<Func<reg_abroad, bool>> waiteQuery1 = c => true;
-            //if (waiter_id != null && waiter_id.Value > 0)
+            //Expression<Func<reg_abroad, bool>> userQuery1 = c => true;
+            //if (hasInspect == null)
             //{
-            //    waiteQuery1 = c => (c.waiter_id == waiter_id);
+            //    if (hasCompany == null)
+            //    {
+            //        if (hasDepart != null)
+            //        {
+            //            userQuery1 = c => c.organization_id == deptId;
+            //        }
+            //        else
+            //        {
+            //            userQuery1 = c => (c.salesman_id == userId || c.assistant_id == userId);
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    if (hasCompany == null)
+            //    {
+            //        if (hasDepart != null)
+            //        {
+            //            userQuery1 = c => c.organization_id == deptId;
+            //        }
+            //        else
+            //        {
+            //            userQuery1 = c => c.waiter_id == userId;
+            //        }
+            //    }
             //}
 
-            Expression<Func<reg_abroad, bool>> userQuery1 = c => true;
-            if (hasInspect == null)
+            //int? waiter_id, int? salesman_id, string name
+            Expression<Func<reg_abroad, bool>> waiterQuery1 = c => true;
+            if (waiter_id != null)
             {
-                if (hasCompany == null)
-                {
-                    if (hasDepart != null)
-                    {
-                        userQuery1 = c => c.organization_id == deptId;
-                    } else
-                    {
-                        userQuery1 = c => (c.salesman_id == userId || c.assistant_id == userId);
-                    }
-                }                
-            } else
+                waiterQuery1 = c => c.waiter_id == waiter_id;
+            }
+            Expression<Func<reg_abroad, bool>> salesmanQuery1 = c => true;
+            if (salesman_id != null)
             {
-                if (hasCompany == null)
-                {
-                    if (hasDepart != null)
-                    {
-                        userQuery1 = c => c.organization_id == deptId;
-                    }
-                    else
-                    {
-                        userQuery1 = c => c.waiter_id == userId;
-                    }
-                }
+                salesmanQuery1 = c => c.salesman_id == salesman_id;
+            }
+            Expression<Func<reg_abroad, bool>> nameQuery1 = c => true;
+            if (!string.IsNullOrEmpty(name))
+            {
+                nameQuery1 = c => (c.name_cn.Contains(name)  || c.name_en.Contains(name));
+            }
+            Expression<Func<reg_abroad, bool>> areaQuery1 = c => true;
+            if (!string.IsNullOrEmpty(area))
+            {
+                areaQuery1 = c => c.code.Contains(area);
             }
 
-
-            var abroads = Uof.Ireg_abroadService.GetAll(condition1).Where(customerQuery1).Where(userQuery1).Select(a => new AnnualWarning
+            var abroads = Uof.Ireg_abroadService.GetAll(condition1).Where(customerQuery1)
+                .Where(waiterQuery1)
+                .Where(salesmanQuery1)
+                .Where(nameQuery1)
+                .Where(areaQuery1)
+                .Select(a => new AnnualWarning
             {
                 id = a.id,
                 customer_id = a.customer_id,
@@ -148,7 +170,6 @@ namespace WebCenter.Web.Controllers
                 customer_code = a.customer.code,
                 order_code = a.code,
                 order_name = a.name_cn ?? a.name_en,
-                //order_name_en = a.name_en,
                 order_type = "reg_abroad",
                 order_type_name = "境外注册",
                 saleman = a.member4.name,
@@ -180,43 +201,64 @@ namespace WebCenter.Web.Controllers
                 {
                     customerQuery2 = c => (c.customer_id == customer_id);
                 }
-                //Expression<Func<reg_internal, bool>> waiteQuery2 = c => true;
-                //if (waiter_id != null && waiter_id.Value > 0)
+
+                //Expression<Func<reg_internal, bool>> userQuery2 = c => true;
+                //if (hasInspect == null)
                 //{
-                //    waiteQuery2 = c => (c.waiter_id == waiter_id);
+                //    if (hasCompany == null)
+                //    {
+                //        if (hasDepart != null)
+                //        {
+                //            userQuery2 = c => c.organization_id == deptId;
+                //        }
+                //        else
+                //        {
+                //            userQuery2 = c => (c.salesman_id == userId || c.assistant_id == userId);
+                //        }
+                //    }
+                //}
+                //else
+                //{
+                //    if (hasCompany == null)
+                //    {
+                //        if (hasDepart != null)
+                //        {
+                //            userQuery2 = c => c.organization_id == deptId;
+                //        }
+                //        else
+                //        {
+                //            userQuery2 = c => c.waiter_id == userId;
+                //        }
+                //    }
                 //}
 
-                Expression<Func<reg_internal, bool>> userQuery2 = c => true;
-                if (hasInspect == null)
+                Expression<Func<reg_internal, bool>> waiterQuery2 = c => true;
+                if (waiter_id != null)
                 {
-                    if (hasCompany == null)
-                    {
-                        if (hasDepart != null)
-                        {
-                            userQuery2 = c => c.organization_id == deptId;
-                        }
-                        else
-                        {
-                            userQuery2 = c => (c.salesman_id == userId || c.assistant_id == userId);
-                        }
-                    }
+                    waiterQuery2 = c => c.waiter_id == waiter_id;
                 }
-                else
+                Expression<Func<reg_internal, bool>> salesmanQuery2 = c => true;
+                if (salesman_id != null)
                 {
-                    if (hasCompany == null)
-                    {
-                        if (hasDepart != null)
-                        {
-                            userQuery2 = c => c.organization_id == deptId;
-                        }
-                        else
-                        {
-                            userQuery2 = c => c.waiter_id == userId;
-                        }
-                    }
+                    salesmanQuery2 = c => c.salesman_id == salesman_id;
+                }
+                Expression<Func<reg_internal, bool>> nameQuery2 = c => true;
+                if (!string.IsNullOrEmpty(name))
+                {
+                    nameQuery2 = c => c.name_cn.Contains(name);
+                }
+                Expression<Func<reg_internal, bool>> areaQuery2 = c => true;
+                if (!string.IsNullOrEmpty(area))
+                {
+                    areaQuery2 = c => c.code.Contains(area);
                 }
 
-                var internas = Uof.Ireg_internalService.GetAll(condition2).Where(customerQuery2).Where(userQuery2).Select(a => new AnnualWarning
+                var internas = Uof.Ireg_internalService.GetAll(condition2).Where(customerQuery2)
+                    .Where(waiterQuery2)
+                    .Where(salesmanQuery2)
+                    .Where(nameQuery2)
+                    .Where(areaQuery2)
+                    .Select(a => new AnnualWarning
                 {
                     id = a.id,
                     customer_id = a.customer_id,
@@ -258,43 +300,64 @@ namespace WebCenter.Web.Controllers
             {
                 customerQuery3 = c => (c.customer_id == customer_id);
             }
-            //Expression<Func<trademark, bool>> waiteQuery3 = c => true;
-            //if (waiter_id != null && waiter_id.Value > 0)
+
+            //Expression<Func<trademark, bool>> userQuery3 = c => true;
+            //if (hasInspect == null)
             //{
-            //    waiteQuery3 = c => (c.waiter_id == waiter_id);
+            //    if (hasCompany == null)
+            //    {
+            //        if (hasDepart != null)
+            //        {
+            //            userQuery3 = c => c.organization_id == deptId;
+            //        }
+            //        else
+            //        {
+            //            userQuery3 = c => (c.salesman_id == userId || c.assistant_id == userId);
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    if (hasCompany == null)
+            //    {
+            //        if (hasDepart != null)
+            //        {
+            //            userQuery3 = c => c.organization_id == deptId;
+            //        }
+            //        else
+            //        {
+            //            userQuery3 = c => c.waiter_id == userId;
+            //        }
+            //    }
             //}
 
-            Expression<Func<trademark, bool>> userQuery3 = c => true;
-            if (hasInspect == null)
+            Expression<Func<trademark, bool>> waiterQuery3 = c => true;
+            if (waiter_id != null)
             {
-                if (hasCompany == null)
-                {
-                    if (hasDepart != null)
-                    {
-                        userQuery3 = c => c.organization_id == deptId;
-                    }
-                    else
-                    {
-                        userQuery3 = c => (c.salesman_id == userId || c.assistant_id == userId);
-                    }
-                }
+                waiterQuery3 = c => c.waiter_id == waiter_id;
             }
-            else
+            Expression<Func<trademark, bool>> salesmanQuery3 = c => true;
+            if (salesman_id != null)
             {
-                if (hasCompany == null)
-                {
-                    if (hasDepart != null)
-                    {
-                        userQuery3 = c => c.organization_id == deptId;
-                    }
-                    else
-                    {
-                        userQuery3 = c => c.waiter_id == userId;
-                    }
-                }
+                salesmanQuery3 = c => c.salesman_id == salesman_id;
+            }
+            Expression<Func<trademark, bool>> nameQuery3 = c => true;
+            if (!string.IsNullOrEmpty(name))
+            {
+                nameQuery3 = c => c.name.Contains(name);
+            }
+            Expression<Func<trademark, bool>> areaQuery3 = c => true;
+            if (!string.IsNullOrEmpty(area))
+            {
+                areaQuery3 = c => c.code.Contains(area);
             }
 
-            var trademarks = Uof.ItrademarkService.GetAll(condition3).Where(customerQuery3).Where(userQuery3).Select(a => new AnnualWarning
+            var trademarks = Uof.ItrademarkService.GetAll(condition3).Where(customerQuery3)
+                .Where(waiterQuery3)
+                .Where(salesmanQuery3)
+                .Where(nameQuery3)
+                .Where(areaQuery3)
+                .Select(a => new AnnualWarning
             {
                 id = a.id,
                 customer_id = a.customer_id,
@@ -318,9 +381,7 @@ namespace WebCenter.Web.Controllers
             }
             #endregion
 
-            #region 专利注册
-            //Expression<Func<patent, bool>> condition4 = c => c.status == 4 && c.submit_review_date.Value.Month - Month1 >= 1 && nowYear > c.submit_review_date.Value.Year && (c.annual_year == null || (c.annual_year != null && c.annual_year < nowYear));
-
+            #region 专利注册            
             var patentPeriodSetting = Uof.IsettingService.GetAll(s => s.name == "PATENT_PERIOD").Select(s => s.value).FirstOrDefault();
             int patentPeriod = 0;
             int.TryParse(patentPeriodSetting, out patentPeriod);
@@ -335,44 +396,64 @@ namespace WebCenter.Web.Controllers
             {
                 customerQuery4 = c => (c.customer_id == customer_id);
             }
-            //Expression<Func<patent, bool>> waiteQuery4 = c => true;
-            //if (waiter_id != null && waiter_id.Value > 0)
+
+            //Expression<Func<patent, bool>> userQuery4 = c => true;
+            //if (hasInspect == null)
             //{
-            //    waiteQuery4 = c => (c.waiter_id == waiter_id);
+            //    if (hasCompany == null)
+            //    {
+            //        if (hasDepart != null)
+            //        {
+            //            userQuery3 = c => c.organization_id == deptId;
+            //        }
+            //        else
+            //        {
+            //            userQuery3 = c => (c.salesman_id == userId || c.assistant_id == userId);
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    if (hasCompany == null)
+            //    {
+            //        if (hasDepart != null)
+            //        {
+            //            userQuery3 = c => c.organization_id == deptId;
+            //        }
+            //        else
+            //        {
+            //            userQuery3 = c => c.waiter_id == userId;
+            //        }
+            //    }
             //}
 
-            Expression<Func<patent, bool>> userQuery4 = c => true;
-            if (hasInspect == null)
+            Expression<Func<patent, bool>> waiterQuery4 = c => true;
+            if (waiter_id != null)
             {
-                if (hasCompany == null)
-                {
-                    if (hasDepart != null)
-                    {
-                        userQuery3 = c => c.organization_id == deptId;
-                    }
-                    else
-                    {
-                        userQuery3 = c => (c.salesman_id == userId || c.assistant_id == userId);
-                    }
-                }
+                waiterQuery3 = c => c.waiter_id == waiter_id;
             }
-            else
+            Expression<Func<patent, bool>> salesmanQuery4 = c => true;
+            if (salesman_id != null)
             {
-                if (hasCompany == null)
-                {
-                    if (hasDepart != null)
-                    {
-                        userQuery3 = c => c.organization_id == deptId;
-                    }
-                    else
-                    {
-                        userQuery3 = c => c.waiter_id == userId;
-                    }
-                }
+                salesmanQuery3 = c => c.salesman_id == salesman_id;
+            }
+            Expression<Func<patent, bool>> nameQuery4 = c => true;
+            if (!string.IsNullOrEmpty(name))
+            {
+                nameQuery3 = c => c.name.Contains(name);
+            }
+            Expression<Func<patent, bool>> areaQuery4 = c => true;
+            if (!string.IsNullOrEmpty(area))
+            {
+                areaQuery4 = c => c.code.Contains(area);
             }
 
-
-            var patents = Uof.IpatentService.GetAll(condition4).Where(customerQuery4).Where(userQuery4).Select(a => new AnnualWarning
+            var patents = Uof.IpatentService.GetAll(condition4)
+                .Where(waiterQuery4)
+                .Where(salesmanQuery4)
+                .Where(nameQuery4)
+                .Where(areaQuery4)
+                .Select(a => new AnnualWarning
             {
                 id = a.id,
                 customer_id = a.customer_id,
