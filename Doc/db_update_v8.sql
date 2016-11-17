64f67b6243ec671029d344e7689dd6695e74a791
ZhangChun <liuman2@163.com>
2016-11-17 14:31:02 +0800

    .

---
+++ Doc/db_update_v8.sql
@@ -0,0 +1,4 @@
+INSERT INTO `menu` VALUES ('40', '15', 'inbox', 'fa fa-inbox', '收件管理', '36');
+
+alter table reg_abroad modify column director varchar(100);
+alter table reg_internal modify column director varchar(100);
